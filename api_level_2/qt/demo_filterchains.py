ce6951d46f1c7eb05550a60d8bd38f7921f1e6a0
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2018-11-05 18:17:37 +0200

    etc

--- api_level_2/qt/demo_filterchains.py
+++ api_level_2/qt/demo_filterchains.py
@@ -12,7 +12,7 @@ Valkka Python3 examples library is free software: you can redistribute it and/or
 @file    demo_filterchains.py
 @author  Sampsa Riikonen
 @date    2018
-@version 0.7.1 
+@version 0.8.0 
 @brief   
 """
 import sys
--- aux/run_tutorial.bash
+++ aux/run_tutorial.bash
@@ -7,7 +7,7 @@
 # API level 1
 
 # the shmem examples (lesson_4_*.py) are interactive
-lis="lesson_1_a lesson_1_b lesson_1_c lesson_2_a lesson_3_a lesson_3_b lesson_3_c lesson_5_a lesson_5_b lesson_6_a lesson_7_a lesson_8_a"
+lis="lesson_1_a lesson_1_b lesson_1_c lesson_2_a lesson_3_a lesson_3_b lesson_3_c lesson_5_a lesson_5_b lesson_6_a lesson_7_a lesson_8_a lesson_9"
 
 rm -f test.out
 
