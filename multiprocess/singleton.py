95c7c7b47ce14b3e99459aca4e0b0839fdc7c2d3
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2024-08-06 12:42:18 +0300

    etc

--- multiprocess/singleton.py
+++ multiprocess/singleton.py
@@ -3,4 +3,3 @@ from valkka.multiprocess.sync import EventGroup
 from multiprocessing import Lock
 
 event_fd_group_1 = EventGroup(100, EventFd) # unix file descriptors to sync between SHMEM SERVER and CLIENT
-lock = Lock()
